fa5ae8e6b05c556c058fb46bb86f1f8edc35c2db
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-12-05 03:35:53 +0900

    feat: 가격을 `comma` 로 구분해주는 유틸 함수 구현

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -13,10 +13,22 @@ export const parseQueryString = (search: string): Record<string, string> => {
     }, {} as Record<string, string>);
 };
 
-export const validateCorrectIdValue = (inputValue: string) => {
+export const validateCorrectIdValue = (inputValue: string): boolean => {
   return /^([A-Z]|[a-z]|[\d]){5,30}$/.test(inputValue);
 };
 
-export const validateCorrectPasswordValue = (inputValue: string) => {
+export const validateCorrectPasswordValue = (inputValue: string): boolean => {
   return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/.test(inputValue);
+};
+
+export const getPriceWithComma = (price: number): string => {
+  const reversePrice = price.toString().split("").reverse();
+  let reverseCommaPrice = "";
+
+  reversePrice.forEach((value, index) => {
+    if (index % 3 === 0 && index !== 0) reverseCommaPrice += ",";
+    reverseCommaPrice += value;
+  });
+
+  return reverseCommaPrice.split("").reverse().join("") + "원";
 };

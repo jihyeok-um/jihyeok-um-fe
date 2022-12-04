476d09148cf94ad1797bc516d0976f5930900a10
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-12-05 03:32:09 +0900

    feat: axios 파일 하나로 합치기, 상품 목록 요청 함수 구현

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,10 +1,19 @@
 import axios from "axios";
 import { LoginInfo, LoginResponse } from "../types/login";
+import { ProductsResponse } from "../types/product";
 
 export const requestPostLogin = async (
   loginInfo: LoginInfo
 ): Promise<LoginResponse> => {
   const res = await axios.post("/login", loginInfo);
+
+  return res.data.data;
+};
+
+export const requestGetProducts = async (
+  pageIndex: number
+): Promise<ProductsResponse> => {
+  const res = await axios.get(`/products?page=${pageIndex}&size=10`);
 
   return res.data.data;
 };

5fd7c5a1009aeca059707c1320da64b288348969
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-12-06 23:23:31 +0900

    feat: 로딩 상태를 위한 Spinner 컴포넌트 구현

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,24 +1,27 @@
 import type { NextPage } from "next";
 import styled from "styled-components";
 import Error from "../../components/Error";
+import Spinner from "../../components/Spinner";
 
 import useProduct from "../../hooks/useProduct";
 import { getPriceWithComma } from "../../utilities";
 
 const ProductDetailPage: NextPage = () => {
-  const { product, productError } = useProduct();
+  const { product, productLoading, productError } = useProduct();
 
-  if (productError || !product)
-    return <Error errorText="존재하지 않는 상품입니다."></Error>;
+  if (productLoading) return <Spinner />;
+  if (productError) return <Error errorText="존재하지 않는 상품입니다." />;
 
   return (
     <>
-      {product.thumbnail && <S.Thumbnail src={product.thumbnail} />}
-      {!product.thumbnail && <S.Thumbnail src={"/defaultThumbnail.jpg"} />}
-      <S.ProductInfoWrapper>
-        <S.Name>{product.name}</S.Name>
-        <S.Price>{getPriceWithComma(product.price)}</S.Price>
-      </S.ProductInfoWrapper>
+      {product?.thumbnail && <S.Thumbnail src={product.thumbnail} />}
+      {!product?.thumbnail && <S.Thumbnail src={"/defaultThumbnail.jpg"} />}
+      {product && (
+        <S.ProductInfoWrapper>
+          <S.Name>{product.name}</S.Name>
+          <S.Price>{getPriceWithComma(product.price)}</S.Price>
+        </S.ProductInfoWrapper>
+      )}
     </>
   );
 };

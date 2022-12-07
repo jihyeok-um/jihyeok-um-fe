4319cdc51c187a1b851ceea4f0e4f2dc4d0d2d85
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-12-07 23:25:30 +0900

    refactor: html에 title, meta 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>HAUS</title>
+        <meta name="description" content="식스샵 프론트엔드 개발자 채용 과제" />
+      </Head>
       <body>
         <Main />
         <NextScript />
